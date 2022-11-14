30bf3dfb7fafa507a52566db0543cc250b939992
Suyeol Yun <syyun@mit.edu>
2022-11-14 17:50:31 -0500

    complete one category of interest case

--- main.py
+++ main.py
@@ -34,11 +34,16 @@ def plot_results(solvers, solver_names, figname):
     # Sub.fig. 1: Regrets in time.
     for i, s in enumerate(solvers):
         ax1.plot(range(len(s.regrets)), s.regrets, label=solver_names[i])
+        ax1.plot(
+            range(len(s.regrets)),
+            [s.bandit.best_proba * i for i in range(len(s.regrets))],
+            label=solver_names[i],
+        )
         pass
 
     ax1.set_xlabel("Time step")
     ax1.set_ylabel("Cumulative regret")
-    ax1.legend(loc=9, bbox_to_anchor=(1.82, -0.25), ncol=5)
+    # ax1.legend(loc=9, bbox_to_anchor=(1.82, -0.25), ncol=5)
     ax1.grid("k", ls="--", alpha=0.3)
 
     # Sub.fig. 2: Probabilities estimated by solvers.
