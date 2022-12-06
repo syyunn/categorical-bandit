8d8d29fe52b057b1449642a876fb1f1a90ba93e5
Suyeol Yun <syyun@mit.edu>
2022-12-06 18:24:58 -0500

    add supercloud submit

---
+++ submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ 
+
+# Slurm sbatch options
+#SBATCH -o myScript.sh.log-%j
+
+ 
+
+# # Loading the required module
+# source /etc/profile
+# module load anaconda/2020a
+
+ 
+
+# Run the script
+python main.py --ptemp 0 --expid 0
+python main.py --ptemp 0 --expid 1
